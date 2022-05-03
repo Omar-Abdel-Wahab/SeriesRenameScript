b99aa7262f1219cfdc5ebc7fb34e7cba9f6d7ec0
Omar-Abdel-Wahab <16p6034@eng.asu.edu.eg>
2022-05-03 21:41:52 +0200

    Restructured the code to procedural paradigm
    Code was previously one large function. Now, it is composed of multiple functions

--- main.py
+++ main.py
@@ -1,34 +1,55 @@
 import os
+import sys
+
+END_PROGRAM = ['q', 'Q']
 
 
-def rename_files(ans):
+def rename_files(path, name, season):
+    change_directory(path)
+    files = get_files()
+    _, extension = os.path.splitext(files[0])
+
+    for episode_number, file in enumerate(files, start=1):
+        # File name format = "Series_Name - Season_Number - Episode_Number.extension"
+        os.rename(file, f'{name} - S{int(season):02} - E{episode_number:02}{extension}')
+
+
+def change_directory(path):
     try:
-        # Change the working directory to the path provided by the user
-        os.chdir(ans)
-    except OSError as e:
-        print(e)
-    # Get all files and folders in this directory
-    files = os.listdir(ans)
-    name = input("Please enter the series name you would like for the files: ")
-    season = input("And the season: ")
-    print("Working on it..")
-    exts = []
+        os.chdir(path)
+    except FileNotFoundError:
+        print("Please check that the entered folder exists or use an absolute path")
+        sys.exit()
+
+
+def get_files():
+    files = os.listdir()
+
     for file in files:
-        file_path = os.path.join(os.getcwd(), file)
-        # Check if there is a sub-folder in the current folder. If so, remove it
-        if os.path.isdir(file_path):
+        if os.path.isdir(file):
             files.remove(file)
-        else:
-            _, file_ext = os.path.splitext(file_path)
-            exts.append(file_ext)
-    for episode_number, file in enumerate(files):
-        # Rename each file to this format "Series Name - Season - Episode. Associated extension"
-        os.rename(file, f'{name} - S{int(season):02} - E{episode_number + 1}{exts[episode_number]}')
-    print("Done, Enjoy the series!")
+
+    return files
+
+
+def ask_for_input_or_exit():
+    answer = input("Enter a folder path or 'q' to exit: ")
+    if answer in END_PROGRAM:
+        return answer, None, None
+
+    name = input("Enter the series name you would like for the files: ")
+    season = input("And the season: ")
+
+    return answer, name, season
 
 
 if __name__ == "__main__":
-    answer = input("Please enter a folder path or 'q' to exit: ")
-    while answer != 'q':
-        rename_files(answer)
 
+    while True:
+        folder_path, basename, season_number = ask_for_input_or_exit()
+        if folder_path in END_PROGRAM:
+            break
+        else:
+            print("Working on it..")
+            rename_files(folder_path, basename, season_number)
+            print("Done, Enjoy the series!")
